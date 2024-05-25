3ffd9f0416b80f93d9cf36f308684c95b45ab154
aleledesma <aleledesma1212@gmail.com>
2024-05-25 17:12:25 -0300

    fix: add specific database to connect

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -5,7 +5,7 @@ const connectDB = async () => {
     throw new Error("No existe la URL de la base de datos")
   }
   try {
-    await mongoose.connect(process.env.MONGODB_URL)
+    await mongoose.connect(process.env.MONGODB_URL, { dbName: "dev" })
     console.log("Conectado a la Base de Datos")
   } catch (error) {
     console.log("Hubo un error al conectarse a la base de datos", error)
