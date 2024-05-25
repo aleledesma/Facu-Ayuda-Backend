b0f91cb3dfb8feebb030745aaf463468585068a0
aleledesma <aleledesma1212@gmail.com>
2024-05-25 10:30:16 -0300

    fix: delete dist file

--- dist/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getLoginCode, login, registerUser } from "../controllers/auth.js";
-import { schemaValidator } from "../middlewares/schemaValidator.js";
-import { getCodeSchema, loginSchema, registerSchema, } from "../schemas/auth.schemas.js";
-const router = express.Router();
-router.post("/login/:email/code", schemaValidator(getCodeSchema), getLoginCode);
-router.post("/login/:email", schemaValidator(loginSchema), login);
-router.post("/register", schemaValidator(registerSchema), registerUser);
-export default router;
--- dist/src/helpers/genNumCode.js
+++
@@ -1,9 +0,0 @@
-const genNumCode = (lenght) => {
-    let randomCode = "";
-    for (let index = 0; index <= lenght - 1; index++) {
-        const number = Math.floor(Math.random() * 10);
-        randomCode += number;
-    }
-    return randomCode;
-};
-export default genNumCode;
